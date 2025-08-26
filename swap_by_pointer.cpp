316a692c963bdfe947ab24284044b9e5e91e7748
Rachit3106 <rachitdeshpande14@gmail.com>
2025-08-26 18:10:27 +0530

    Update POINTER OPERATIONS

---
+++ swap_by_pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void swap(int *x,int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+    cout << "After swapping: x = " << *x << ", y = " << *y << endl;
+}
+int main(){
+    int a=5,b=10;
+    swap(&a,&b);
+    cout << "In main: a = " << a << ", b = " << b << endl;
+    return 0;
+}
